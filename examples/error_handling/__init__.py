0078cd79c6ad4df51ac634a94078231d4158c243
nxtlo <dhmony-99@hotmail.com>
2025-07-06 10:50:44 +0300

    update error_handling example

--- examples/error_handling/__init__.py
+++ examples/error_handling/__init__.py
@@ -11,29 +11,35 @@ from sain.result import Result, Ok, Err
 from sain.option import Option, Some
 
 from enum import Enum
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
-# errors are basic enums with 0 overhead, not exception, their job is to
-# give the user a cozy way to handle errors. with 0 try/except.
+# * here, we're making the errors basic enums, their job is to give the user
+# * a ergonomic way to handle errors.
 
 
-class MailError(Enum, str):
+class MailError(str, Enum):
     """Possible errors when sending an email."""
 
     EMAIL_PARSE_ERROR = "failed to parse the email address"
     SERVER_UNAVAILABLE = "Mail server is unavailable"
     CONTENT_TOO_LONG = "Email content exceeds the maximum length"
 
+    def __repr__(self) -> str:
+        # make the repr returns the value of the enum variant instead of the name.
+        return self.value
+
 
 @dataclass
 class Email:
+    """Represents an email to send."""
+
     from_: str
-    """who's sending the email"""
+    """who's sending the email."""
     to: str
     """who's receiving the email"""
     subject: str = "(no subject)"
-    """subject of the email"""
-    content: Option[str] = field(default=Some(None))
+    """subject of the email."""
+    content: Option[str] = Some(None)
     """content of the email, can be None if not provided."""
 
     # Send an email, returning `Ok(None)` if all is good,
@@ -82,7 +88,10 @@ match email.send():
             case err:
                 print(f"failed to send an email: {err=}")
 
-# or just unwrap it if you need the result, not recommended since it causes runtime exceptions.
-result = email.send().unwrap()
+# NOTE - This block will raise.
+if False:
+    # or just unwrap it if you need the result, not recommended since it causes runtime exceptions.
+    result = email.send().unwrap()
+
 # instead, handle it with "unwrap_or", it will just log the error, if any.
 result = email.send().unwrap_or_else(logging.error)
